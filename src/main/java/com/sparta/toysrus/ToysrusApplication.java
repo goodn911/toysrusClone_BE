8c3c76817ceb66dc6efa82bb0acd2b07fb01878e
goodn911 <90505402+goodn911@users.noreply.github.com>
2021-12-13 14:25:37 +0900

    첫 커밋

---
+++ src/main/java/com/sparta/toysrus/ToysrusApplication.java
@@ -0,0 +1,13 @@
+package com.sparta.toysrus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ToysrusApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ToysrusApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sparta/toysrus/ToysrusApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sparta.toysrus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ToysrusApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
